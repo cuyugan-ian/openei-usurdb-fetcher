3d2916cfebfba93e85b1e8016d7fdf16e1f36541
Ian Cuyugan <cuyugan.ian.v@gmail.com>
2018-11-07 22:21:43 +0800

    Modified log4j2 config file and replaced System.out to logger.

--- src/net/modsolar/web/Main.java
+++ src/net/modsolar/web/Main.java
@@ -14,8 +14,14 @@ import java.io.IOException;
 import org.json.simple.parser.ParseException;
 import com.chilkatsoft.*;
 import net.modsolar.constant.OpenEI_Format;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
+    private final String CSV_FILE = "resources/zip.csv";
+    private final String API_KEY = "rAhpJ0Tjtg2q6YAdYlReaZtl4TntH1Mi4BONTqhj";
+    private final UtilityRateFetcher RATE_FETCHER = new UtilityRateFetcher(API_KEY, OpenEI_Format.JSON, 3);
 	
     static {
         try {
@@ -28,44 +34,44 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            System.out.println("Starting Sequence");
+            LOGGER.info("Starting Sequence");
             new Main().initiateRateChecker();
         } catch (ParseException | IOException e) {
-            System.err.print(e);
+            LOGGER.error(e);
         }
     }
         
-	private void initiateRateChecker() throws IOException, ParseException {
-		ConfigUtil configUtil = new ConfigUtil("app.properties");
-		configUtil.load();
-		int currentIndex = Integer.parseInt(configUtil.getValue("current_index"));
-		final int maxRow = Integer.parseInt(configUtil.getValue("max_row"));
-		
-		if (currentIndex > maxRow) {
-			System.out.println("Sequence Complete!");
-			System.exit(1);
-		}
-		
-		final String apiKey = "YOUR_API_KEY";
-		UtilityRateFetcher fetcher = new UtilityRateFetcher(apiKey, OpenEI_Format.JSON, 3);
-		
-		FileReader reader = new FileReader();
-		final String csvFile = "resources/zip.csv";
-		final CkCsv ckCsv = reader.loadCSV(csvFile, true);
-		final String API = "https://api.openei.org/utility_rates?version=3&format=json&"
-				+ "api_key="+apiKey+"&sector=Residential&approved=true&direction=desc"
-				+ "&orderby=startdate&servicetype=bundled&detail=full&limit=10&address=";
-		
-		while (currentIndex <= maxRow) {
-			String zipCode = ckCsv.getCell(currentIndex, 0);
-			System.out.println("("+currentIndex + ") Now Processing: "+zipCode);
-			String httpsURI = API + zipCode;
-			String value = fetcher.getRateByZipCode(httpsURI, configUtil, currentIndex);
-			ckCsv.SetCell(currentIndex, 4, value);
-			ckCsv.SaveFile(csvFile);
-			currentIndex++;
-		}
-	}
+    private void initiateRateChecker() throws IOException, ParseException {
+        ConfigUtil configUtil = new ConfigUtil("app.properties");
+        configUtil.load();
+        int currentIndex = Integer.parseInt(configUtil.getValue("current_index"));
+        final int maxRow = Integer.parseInt(configUtil.getValue("max_row"));
+
+        if (currentIndex > maxRow) {
+            LOGGER.info("Sequence Complete!");
+            System.exit(1);
+        }
+
+        FileReader reader = new FileReader();
+        final CkCsv ckCsv = reader.loadCSV(CSV_FILE, true);
+        final String API = "https://api.openei.org/utility_rates?version=3&format=json&"
+                        + "api_key="+API_KEY+"&sector=Residential&approved=true&direction=desc"
+                        + "&orderby=startdate&servicetype=bundled&detail=full&limit=10&address=";
+
+        while (currentIndex <= maxRow) {
+            String zipCode = ckCsv.getCell(currentIndex, 0);
+            LOGGER.info("("+currentIndex + ") ZIP CODE: "+zipCode);
+            String httpsURI = API + zipCode;
+            checkRateAvailability(httpsURI, configUtil, currentIndex, ckCsv);
+            currentIndex++;
+        }
+    }
+
+    private void checkRateAvailability(String httpsURI, ConfigUtil configUtil, 
+            int currentIndex, CkCsv ckCsv) throws ParseException, IOException {
+        String value = RATE_FETCHER.checkRateAvailability(httpsURI, configUtil, currentIndex);
+        ckCsv.SetCell(currentIndex, 4, value);
+        ckCsv.SaveFile(CSV_FILE);
+    }
 	
 }
-
--- src/net/modsolar/web/UtilityRateFetcher.java
+++ src/net/modsolar/web/UtilityRateFetcher.java
@@ -11,7 +11,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -23,17 +22,20 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import net.modsolar.constant.OpenEI_Format;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 class UtilityRateFetcher {
-	final String API_KEY;
-	final OpenEI_Format FORMAT;
-	final int VERSION;
-	
-	UtilityRateFetcher(String apiKey, OpenEI_Format format, int version) {
-		this.API_KEY = apiKey;
-		this.FORMAT = format;
-		this.VERSION = version;
-	}
+    private final Logger LOGGER = LogManager.getLogger(UtilityRateFetcher.class);
+    private final String API_KEY;
+    private final OpenEI_Format FORMAT;
+    private final int VERSION;
+
+    UtilityRateFetcher(String apiKey, OpenEI_Format format, int version) {
+        this.API_KEY = apiKey;
+        this.FORMAT = format;
+        this.VERSION = version;
+    }
 	
 	/*
 	 * For list of parameters for OpenEI's USURDB check link below
@@ -53,46 +55,46 @@ class UtilityRateFetcher {
 		return null;
 	}
 	
-	String getRateByZipCode(String httpsURI, ConfigUtil configUtil, int currIndex) throws IOException, ParseException {
-		httpsURI = Objects.requireNonNull(httpsURI, "Required URL of OpenEI's USURDB!");
-		StringBuffer utilities = this.getUtilities(httpsURI, configUtil, currIndex);
-		if (null == utilities) {
-			return "NO";
-		}
-		JSONParser parser = new JSONParser();
-		JSONObject jobj = (JSONObject)parser.parse(utilities.toString());
-		JSONArray jsonArr = (JSONArray)jobj.get("errors");
-		return (null == jsonArr) ? "YES" : "NO";
-	}
+    String checkRateAvailability(String httpsURI, ConfigUtil configUtil, int currIndex) throws IOException, ParseException {
+        httpsURI = Objects.requireNonNull(httpsURI, "Required URL of OpenEI's USURDB!");
+        StringBuffer utilities = this.getUtilities(httpsURI, configUtil, currIndex);
+        if (null == utilities) {
+            return "NO";
+        }
+        JSONParser parser = new JSONParser();
+        JSONObject jobj = (JSONObject)parser.parse(utilities.toString());
+        JSONArray jsonArr = (JSONArray)jobj.get("errors");
+        return (null == jsonArr) ? "YES" : "NO";
+    }
 	
-	private StringBuffer getUtilities(String httpsURI, ConfigUtil configUtil, int currIndex) throws IOException{
-		StringBuffer utilities = null;
-		URL url = new URL(httpsURI);
-		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
-		con.setRequestMethod("GET");
-		
-		if (con.getResponseCode() == 200) {
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			utilities = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-			    utilities.append(inputLine);
-			}
-			inputLine = null;
-			in.close();
-			in = null;
-		} else {
-			System.err.println("Response Code: "+con.getResponseCode());
-			System.out.println("Terminating Sequence");
-			configUtil.setValue("current_index", String.valueOf(currIndex));
-			System.exit(1);
-		}
-		con.disconnect();
-		con = null;
-		url = null;
-		
-		return utilities;
-	}
+    private StringBuffer getUtilities(String httpsURI, ConfigUtil configUtil, int currIndex) throws IOException{
+        StringBuffer utilities = null;
+        URL url = new URL(httpsURI);
+        HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        if (con.getResponseCode() == 200) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            utilities = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                utilities.append(inputLine);
+            }
+            inputLine = null;
+            in.close();
+            in = null;
+        } else {
+            LOGGER.error("Response Code: "+con.getResponseCode());
+            LOGGER.error("Sequence Terminated");
+            configUtil.setValue("current_index", String.valueOf(currIndex));
+            System.exit(1);
+        }
+        con.disconnect();
+        con = null;
+        url = null;
+
+        return utilities;
+    }
 	
 	/*
 	 * in progress 
