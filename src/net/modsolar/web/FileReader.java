2ec67aeccec70f58f004c9d8ae3261e0f46119e3
Ian Cuyugan <cuyugan.ian.v@gmail.com>
2018-11-12 15:41:46 +0800

    Add rate coverage checker.

--- src/net/modsolar/web/FileReader.java
+++ src/net/modsolar/web/FileReader.java
@@ -10,22 +10,56 @@ package net.modsolar.web;
 
 import java.util.Objects;
 import com.chilkatsoft.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class FileReader {
 	
-	/*
-	 * Reads CSV file.
-	 */
-	CkCsv loadCSV(String csvFile, boolean withHeaders) {
-		csvFile = Objects.requireNonNull(csvFile, "Missing required CSV File!");
-		
-		CkCsv ckCsv = new CkCsv();
-		ckCsv.put_HasColumnNames(withHeaders);
-		if (!ckCsv.LoadFile(csvFile)) {
-			throw new NullPointerException("CSV file not found!");
-		}
-		
-		return ckCsv;
-	}
+    /*
+     * Reads CSV file.
+     */
+    CkCsv loadCSV(String csvFile, boolean withHeaders) {
+        csvFile = Objects.requireNonNull(csvFile, "Missing required CSV File!");
 
+        CkCsv ckCsv = new CkCsv();
+        ckCsv.put_HasColumnNames(withHeaders);
+        if (!ckCsv.LoadFile(csvFile)) {
+            throw new NullPointerException("CSV file not found!");
+        }
+
+        return ckCsv;
+    }
+    
+    /*
+     * Counts the value "Yes/No" on a column.
+     * returns Map<String, Integer>
+     */
+    Map<String, Integer> countRateAvailability(CkCsv ckCsv, int column, int maxRow) {
+        if (null == ckCsv) {
+            throw new NullPointerException("CKCSV parameter contains null value!");
+        }
+        
+        final String yes = "YES";
+        final String no = "NO";
+        int yesCount = 0;
+        int noCount = 0;
+        
+        for (int i=0; i<=maxRow; i++) {
+            String cellValue = ckCsv.getCell(i, column);
+            if (null == cellValue || cellValue.trim().isEmpty()) {
+                System.out.println("NULL (index): "+i);
+                continue;
+            } else if (cellValue.equalsIgnoreCase(yes)){
+                yesCount++;
+            } else if (cellValue.equalsIgnoreCase(no)) {
+                noCount++;
+            }
+        }
+        
+        Map<String, Integer> mappedValues = new HashMap<String, Integer>();
+        mappedValues.put(yes, yesCount);
+        mappedValues.put(no, noCount);
+        return mappedValues;
+    }
+    
 }
--- src/net/modsolar/web/Main.java
+++ src/net/modsolar/web/Main.java
@@ -15,6 +15,7 @@ package net.modsolar.web;
 import java.io.IOException;
 import org.json.simple.parser.ParseException;
 import com.chilkatsoft.*;
+import java.util.Map;
 import net.modsolar.constant.Constants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -32,25 +33,25 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        new Main().initiateRateChecker();
+    }
+        
+    private void initiateRateChecker() {
         try {
-            new Main().initiateRateChecker();
+            LOGGER.info("Starting Sequence");
+            ConfigUtil configUtil = ConfigUtil.getInstance();
+            int currentIndex = Integer.parseInt(configUtil.getValue("current_index"));
+            final int maxRow = Integer.parseInt(configUtil.getValue("max_row"));
+
+            if (currentIndex > maxRow) {
+                LOGGER.info("Sequence Complete!");
+                System.exit(1);
+            }
+
+            checkRatePerZipCode(currentIndex, maxRow);
         } catch (ParseException | IOException e) {
             LOGGER.error(e);
         }
-    }
-        
-    private void initiateRateChecker() throws IOException, ParseException {
-        LOGGER.info("Starting Sequence");
-        ConfigUtil configUtil = ConfigUtil.getInstance();
-        int currentIndex = Integer.parseInt(configUtil.getValue("current_index"));
-        final int maxRow = Integer.parseInt(configUtil.getValue("max_row"));
-
-        if (currentIndex > maxRow) {
-            LOGGER.info("Sequence Complete!");
-            System.exit(1);
-        }
-
-        checkRatePerZipCode(currentIndex, maxRow);
     }
     
     private void checkRatePerZipCode(int currentIndex, int maxRow) throws ParseException, IOException {
@@ -76,6 +77,24 @@ public class Main {
         return "https://api.openei.org/utility_rates?version=3&format=json&"
             + "api_key="+Constants.API_KEY+"&sector=Residential&approved=true&direction=desc"
                 + "&orderby=startdate&servicetype=bundled&detail=full&limit=10&address="+zipCode;
+    }
+    
+    private void countRateAvailability() {
+        final int maxRow = 74021;
+        final FileReader reader = new FileReader();
+        final CkCsv ckCsv = reader.loadCSV(Constants.CSV_FILE.toString(), true);
+        
+        Map<String, Integer> mappedValues = reader.countRateAvailability(ckCsv, 4, maxRow);
+        if (null == mappedValues || mappedValues.isEmpty()) {
+            System.out.println("Empty Map!");
+            System.exit(1);
+        }
+        
+        Integer yesCount = (mappedValues.containsKey("YES")) ? mappedValues.get("YES") : null;
+        Integer noCount = (mappedValues.containsKey("NO")) ? mappedValues.get("NO") : null;
+        
+        System.out.println("YES: "+yesCount);
+        System.out.println("NO: "+noCount);
     }
 	
 }
